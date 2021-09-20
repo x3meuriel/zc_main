fd28835fe1225a83061c0aaea10658f871114748
x3meuriel <x3meuriel@users.noreply.github.com>
2021-09-20 19:59:55 +0000

    same_commit

--- routes/api/search.js
+++ routes/api/search.js
@@ -1,7 +1,5 @@
 const express = require('express')
-const router = express.Router();
-const {getFiles} = require('../../controllers/searchController')
+const router = express.Router()
+const { getFiles } = require('../../controllers/searchController')
 
-
-router.get('/files', getFiles);
-
+router.get('/files', getFiles)
